ac7306fac118116458b5b49c188232b6d8241603
Chris Gonzalez <christogonz92@gmail.com>
2024-08-10 10:44:21 +0200

    Initial Commit

---
+++ DashBoard1/DashBoard1App.swift
@@ -0,0 +1,17 @@
+//
+//  DashBoard1App.swift
+//  DashBoard1
+//
+//  Created by Christopher Gonzalez on 2024-08-10.
+//
+
+import SwiftUI
+
+@main
+struct DashBoard1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
